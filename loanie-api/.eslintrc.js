627ff336c9319fe77a7e3550efc47c311be26967
MannieJay <jhall02258@gmail.com>
2018-03-22 19:52:19 -0400

    finalized linter settings

--- loanie-api/.eslintrc.js
+++ loanie-api/.eslintrc.js
@@ -1,28 +1,30 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true
-  },
-  "extends": "airbnb",
-  "parserOptions": {
-    "sourceType": "module"
-  },
-  "rules": {
-    "indent": [
-      "error",
-      "tab"
-    ],
-    "linebreak-style": [
-      "error",
-      "windows"
-    ],
-    "quotes": [
-      "error",
-      "double"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ]
-  }
+	env: {
+		es6: true,
+		node: true,
+	},
+	extends:
+		"airbnb",
+	parserOptions: {
+		sourceType:
+			"module",
+	},
+	rules: {
+		indent: [
+			"error",
+			"space",
+		],
+		"linebreak-style": [
+			"error",
+			"windows",
+		],
+		quotes: [
+			"error",
+			"double",
+		],
+		semi: [
+			"error",
+			"always",
+		],
+	},
 };
--- loanie-api/server.js
+++ loanie-api/server.js
@@ -1,20 +1,18 @@
-//const postData = require('./application-data.js');
+// const postData = require('./application-data.js');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
 const routes = require('./routes/routes');
 
 const port = process.env.PORT || 3030;
 const server = express();
 
-
 const corsOptions = {
-  "origin": "*",
-  "methods": "GET, HEAD, PUT, PATCH, POST, DELETE",
-  "preflightContinue": false,
-  "optionsSuccessStatus": 204
+  origin: '*',
+  methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 
 mongoose.Promise = global.Promise;
@@ -26,8 +24,11 @@ server.use(bodyParser.json());
 server.use(cors());
 
 server.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
