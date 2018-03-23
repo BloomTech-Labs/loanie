698e03fb2b09c41d6baa15e431c777f194e24bcb
Samuel Kim <sams404link@gmail.com>
2018-03-22 23:06:38 -0700

    added working eslint airbnb linter and fixed all issues

--- loanie-view/src/App.js
+++ loanie-view/src/App.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar';
 
-class App extends Component {
-  render() {
-    return (
-      <div className = "App">
-        <Navbar/>
-        <div className = "App-signin-container">
-          <div className = "App-signin-border">
-            <Link className = "App-signin-item" to = "/">Sign In</Link> <Link className = "App-signin-item" to = "/">Sign Up</Link>
-          </div>
-        </div>
-        <div className ="App-text-container">
-          <p className = "App-text-item"> Make your home purchases and mortgages easy and simple!</p>
-        </div>
-        <div className = "button-container">
-          <button className = "button1 button "> Buy Now </button>
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="App-signin-container">
+        <div className="App-signin-border">
+          <Link className="App-signin-item" to="/">Sign In</Link> <Link className="App-signin-item" to="/">Sign Up</Link>
         </div>
       </div>
-    );
-  }
+      <div className="App-text-container">
+        <p className="App-text-item"> Make your home purchases and mortgages easy and simple!</p>
+      </div>
+      <div className="button-container">
+        <button className="button1 button "> Buy Now </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- loanie-view/src/index.js
+++ loanie-view/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-<Router>
-  <div>
-    <Route exact path = "/" component = {App}/> 
-  </div>
-</Router>
-, document.getElementById('root'));
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+    </div>
+  </Router>
+  , document.getElementById('root'),
+);
 registerServiceWorker();
