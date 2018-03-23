d0f6762e50243fa3665b65f01f5bcbdaaee6be9c
Samuel Kim <sams404link@gmail.com>
2018-03-22 23:41:33 -0700

    removed duplicate sign in and sign up links

--- loanie-view/src/App.js
+++ loanie-view/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar';
 
@@ -7,11 +6,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="App-signin-container">
-        <div className="App-signin-border">
-          <Link className="App-signin-item" to="/">Sign In</Link> <Link className="App-signin-item" to="/">Sign Up</Link>
-        </div>
-      </div>
       <div className="App-text-container">
         <p className="App-text-item"> Make your home purchases and mortgages easy and simple!</p>
       </div>
