1b0bf30cf59a1775675adede71cffd5dc16fc5c6
Samuel Kim <sams404link@gmail.com>
2018-03-30 09:38:48 -0700

    saving changes to pull

--- loanie-view/tests/App.test.js
+++ loanie-view/tests/App.test.js
@@ -1,10 +1,8 @@
-
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import { expect } from 'chai';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../src/App';
-
 
 configure({ adapter: new Adapter() });
 const wrapper = shallow(<App />);
