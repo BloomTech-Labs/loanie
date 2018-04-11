41ca98deb5388343489e81c78411b64b03432187
MannieJay <jhall02258@gmail.com>
2018-04-10 21:03:29 -0400

    created default assignments file

---
+++ loanie-view/src/Component/AssignmentDefaults.js
@@ -0,0 +1,223 @@
+export function assignmentDefaults() {
+  const defs = [
+    {
+      text: 'Fill out Loan Application',
+      author: '1',
+      phase: '1',
+      complete: false,
+    },
+    {
+      text: 'Decide on your desired payment',
+      author: '1',
+      phase: '1',
+      complete: false,
+    },
+    {
+      text: 'Look at your finances and see what your down payment amount will likely be (0 - 20%)',
+      author: '1',
+      phase: '1',
+      complete: false,
+    },
+    {
+      text: 'Come up with a basic "wish list" on the property you are looking for',
+      author: '1',
+      phase: '1',
+      complete: false,
+    },
+    {
+      text: 'Decide if you would like to use a Realtor',
+      author: '1',
+      phase: '1',
+      complete: false,
+    },
+    {
+      text: 'Get an idea of your desired location',
+      author: '1',
+      phase: '1',
+      complete: false,
+    },
+    {
+      text: 'Get serious about credit and pre-approved for loan amount',
+      author: '1',
+      phase: '2',
+      complete: false,
+    },
+    {
+      text: 'Give Loan Officer release to check credit',
+      author: '1',
+      phase: '2',
+      complete: false,
+    },
+    {
+      text: 'Give Loan Officer 2 months of check stubbs',
+      author: '1',
+      phase: '2',
+      complete: false,
+    },
+    {
+      text: 'Give Loan Officer 3 months of bank statements',
+      author: '1',
+      phase: '2',
+      complete: false,
+    },
+    {
+      text:
+        'Work on paying bills on time, saving for down payment and cleaning up credit if necessary',
+      author: '1',
+      phase: '2',
+      complete: false,
+    },
+    {
+      text: 'Make sure all previous documents are complete',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: 'Send Loan officer all the following documents:',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: 'Signed disclosures',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: '30 days of pay stubs',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: '3 months of bank statements',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: '2 years tax returns',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: 'most recent tax statements for other property owned',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: '1099 and copy of business license if self-employed',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: 'Explanation of any inquiries on credit (signed and dated)',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: 'Copy of divorce decree and seperation agreement (if applicable)',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: 'Current mortgage statement',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: 'Copy of earnest money check (bank statement showing it cleared)',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: 'Copy of drivers license or other gov. issued ID',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: 'Insurance declaration page',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text:
+        'Residency history for the past 2 years - address and phone number of each landlord or rental company',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: 'Check for the appraisal or bank draft information',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: 'Explanation of any assets used for down payment and closing costs - source of funds',
+      author: '1',
+      phase: '3',
+      complete: false,
+    },
+    {
+      text: 'Consumer must pay for the appraisal $500-600',
+      author: '1',
+      phase: '4',
+      complete: false,
+    },
+    {
+      text: 'Rate can be locked after appraisal - consumer must decide when to lock',
+      author: '1',
+      phase: '4',
+      complete: false,
+    },
+    {
+      text: 'Review final disclosure',
+      author: '1',
+      phase: '5',
+      complete: false,
+    },
+    {
+      text:
+        'Sign and return final disclosure (Ask any questions, get clarification - before signing and returning)',
+      author: '1',
+      phase: '5',
+      complete: false,
+    },
+    {
+      text:
+        'Schedule Closing - Closing cannot take place within 3 days from the date the disclosure is signed',
+      author: '1',
+      phase: '5',
+      complete: false,
+    },
+    {
+      text: 'Show up and be prepared to sign your name at least 75 times',
+      author: '1',
+      phase: '6',
+      complete: false,
+    },
+    {
+      text: 'Get your keys, Move your stuff, Enjoy your life!',
+      author: '1',
+      phase: '6',
+      complete: false,
+    },
+  ];
+  return defs;
+}
+
+export function foo() {
+  return 'bar';
+}
