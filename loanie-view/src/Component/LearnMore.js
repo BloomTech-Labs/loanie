9dbd1b1650a507f269c1ff98a3d550b3a7e768c6
MannieJay <jhall02258@gmail.com>
2018-04-12 15:55:27 -0400

    added background image and styling to LearnMore.js

--- loanie-view/src/Component/LearnMore.js
+++ loanie-view/src/Component/LearnMore.js
@@ -1,28 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SidebarNav from './SideBarNav';
 import Navbar from './Navbar';
 import '../CSS/LearnMore.css';
 
-export default function LearnMore() {
-  return (
-    <div className="learn-more">
-      <Navbar />
-      <SidebarNav />
-      <div className="learn-header">
-        <h1>Why Loanie?</h1>
-        <div className="learn-body">
-          <p>
-            No more phone tag, No more conflicting schedules, No more wondering if you sent the
-            right document or if your documents were accepted. Loanie is here 24 hours a day, 7 days
-            a week to track every step of your mortgage process so you can relax and enjoy life.
-            Loanie breaks the mortgage process down into phases. In each phase your loan officer
-            provides a simple list of assignments. As each assignment is completed you receive a
-            text or email letting you know the loan officer has acknowledge the completion of your
-            assignment. Loanie is the cutting edge tool that takes all the guess work out of getting
-            a mortgage.
-          </p>
+export default class LearnMore extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loginState: false,
+    };
+  }
+  handlePurchase = () => {
+    this.setState({ loginState: true });
+    console.log(this.state.loginState);
+    window.location = '/learn_more';
+  };
+  render() {
+    return (
+      <div className="LearnMore">
+        <Navbar />
+        <SidebarNav />
+        <div className="Learn-container-opacity">
+          <div className="Learn-header">
+            <h1>Why Loanie?</h1>
+          </div>
+          <div className="Learn-text-container">
+            <p className="Learn-text-item">
+              Loanie is the cutting edge tool that takes all the guess work out of getting a
+              mortgage.
+            </p>
+            <p className="Learn-text-list">
+              <ul>
+                <li>No more phone tag.</li>
+                <li>
+                  No more wondering if you sent the correct document or if your documents were
+                  accepted.
+                </li>
+                <li>No more conflicting schedules slowing you down.</li>
+                <li>
+                  Loanie is here 24 hours a day, 7 days a week to track every step of your mortgage
+                  process so you can relax and enjoy life.
+                </li>
+              </ul>
+              <p className="Learn-text-item">
+                Loanie breaks the mortgage process down into phases. In each phase your loan officer
+                provides a simple list of assignments. As each assignment is completed you receive
+                an optional text or email letting you know the loan officer has acknowledge the
+                completion of each assignment.
+              </p>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
