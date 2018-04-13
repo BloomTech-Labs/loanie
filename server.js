088e45d4a78a766d2859039fba8b63704b4ad5a9
MannieJay <jhall02258@gmail.com>
2018-04-13 15:37:28 -0400

    proxy

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ server.use(bodyParser.json());
 
 server.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
