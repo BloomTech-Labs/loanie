bf65ccb382a31b4e6890b3de159b44bc31b7c19a
Samuel Kim <sams404link@gmail.com>
2018-04-13 10:47:32 -0700

    added name to sidebar

--- loanie-view/src/Component/SideBarNav.js
+++ loanie-view/src/Component/SideBarNav.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
+import axios from 'axios';
 import '../CSS/SideBar.css';
 
 export default class SideBarNav extends Component {
   constructor() {
     super();
     this.state = {
+      tokenId: sessionStorage.getItem('tokenId'),
       userType: sessionStorage.getItem('userType'),
+      userName: '',
     };
+  }
+  componentDidMount() {
+    const body = { token: this.state.tokenId };
+    axios
+      .post('http://localhost:3030/user', body)
+      .then((res) => {
+        this.setState({ userName: res.data.name });
+        // console.log('userName', this.state.userName);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
   render() {
     if (this.state.userType === 'managerUser') {
       return (
         <div className="menu-container">
           <Menu>
+            <Link to="/settings">
+              <h4 className="menu-items">
+                <b>{this.state.userName}</b>
+              </h4>
+            </Link>
             <a className="menu-items" href="/create_loan">
               Create Loan
             </a>
@@ -36,6 +57,11 @@ export default class SideBarNav extends Component {
       return (
         <div className="menu-container">
           <Menu>
+            <Link to="/user_settings">
+              <h4 className="menu-items">
+                <b>{this.state.userName}</b>
+              </h4>
+            </Link>
             <a className="menu-items" href="/">
               Home
             </a>
